06b8efa0071e16a796831abddb5cc55ce0ef71da
Ciccioarmory <ciccioarmory@hotmail.it>
2023-12-03 06:42:16 +0100

    Added cool new buttons

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+document.querySelector(".guessMyNumber").addEventListener("click", function () {
+	console.log("Button clicked!");
+	window.location.href = "/guessMyNumber/index.html";
+});
+
+document.querySelector(".wipButton").addEventListener("click", function () {
+	console.log("Button clicked!");
+	window.location.href = "/";
+});
